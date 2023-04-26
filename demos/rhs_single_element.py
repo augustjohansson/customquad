c1d09c47f490769475b9ac9c9f3c871b95b3c9d8
August Johansson <august.johansson@sintef.no>
2023-04-26 23:04:04 +0200

    Rename

--- demos/rhs_single_element.py
+++ demos/rhs_single_element.py
@@ -4,7 +4,7 @@ import ufl
 from ufl import grad, inner
 from mpi4py import MPI
 import numpy
-from numpy import sin, pi
+from numpy import sin, pi, exp
 import FIAT
 from petsc4py import PETSc
 
@@ -12,6 +12,22 @@ from petsc4py import PETSc
 # from shutil import rmtree
 
 # rmtree("/root/.cache/fenics", True)
+
+
+def rhs1(x):
+    return x[0] ** 0
+
+
+def rhs2(x):
+    return x[0]
+
+
+def rhs3(x):
+    return sin(pi * x[0]) * sin(pi * x[1])
+
+
+def rhs4(x):
+    return exp(x[0] * x[1])
 
 
 def ksp_solve(A, b):
@@ -54,30 +70,29 @@ cells = numpy.arange(Nx * Ny)
 V = dolfinx.fem.FunctionSpace(mesh, ("Lagrange", 1))
 v = ufl.TestFunction(V)
 
-# f = dolfinx.Function(V)
-# def rhs(x):
-#     return x[0] #sin(pi*x[0])*sin(pi*x[1])
-# f.interpolate(rhs)
+for k, rhs in enumerate([rhs1, rhs2, rhs3, rhs4]):
+    f = dolfinx.fem.Function(V)
+    f.interpolate(rhs)
 
-# L_eqn = inner(f, v)
-L_eqn = 1 * v
+    L_eqn = inner(f, v)
 
-L = L_eqn * ufl.dx(metadata={"quadrature_rule": "runtime"})
+    # Runtime quadrature
+    L = L_eqn * ufl.dx(metadata={"quadrature_rule": "runtime"})
+    degree = 2
+    q = FIAT.create_quadrature(FIAT.reference_element.UFCQuadrilateral(), degree)
+    qr_pts = q.get_points().flatten()
+    qr_w = q.get_weights().flatten()
+    qr_n = qr_pts  # dummy
+    b = libcutfemx.custom_assemble_vector(L, [(cells, [qr_pts], [qr_w], [qr_n])])
+    print("custom b", b.array)
+    print("custom b norm", numpy.linalg.norm(b.array))
 
-degree = 1
-q = FIAT.create_quadrature(FIAT.reference_element.UFCQuadrilateral(), degree)
-qr_pts = q.get_points().flatten()
-qr_w = q.get_weights().flatten()
-# dummy
-qr_n = qr_pts
+    # Reference
+    Lref = L_eqn * ufl.dx
+    bref = dolfinx.fem.petsc.assemble_vector(dolfinx.fem.form(Lref))
+    print("bref", bref.array)
+    print("bref norm", numpy.linalg.norm(bref.array))
 
-
-b = libcutfemx.custom_assemble_vector(L, [(cells, [qr_pts], [qr_w], [qr_n])])
-print("custom b", b.array)
-print("custom b norm", numpy.linalg.norm(b.array))
-
-# Reference
-Lref = L_eqn * ufl.dx
-bref = dolfinx.fem.petsc.assemble_vector(dolfinx.fem.form(Lref))
-print("bref", bref.array)
-print("bref norm", numpy.linalg.norm(bref.array))
+    assert (
+        numpy.linalg.norm(b.array - bref.array) / numpy.linalg.norm(bref.array) < 1e-10
+    )
