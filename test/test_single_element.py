d669ffa69c52792e1267f88b3a75475b2565f794
August Johansson <august.johansson@sintef.no>
2023-06-03 01:25:35 +0200

    Tile qr for many elements

--- test/test_single_element.py
+++ test/test_single_element.py
@@ -11,7 +11,7 @@ import FIAT
 
 
 def assemble_vector_test(mesh, fiat_element, polynomial_order, quadrature_degree, rhs):
-    # Setup integral
+    # Setup integrand
     V = dolfinx.fem.FunctionSpace(mesh, ("Lagrange", polynomial_order))
     v = ufl.TestFunction(V)
     f = dolfinx.fem.Function(V)
@@ -20,12 +20,13 @@ def assemble_vector_test(mesh, fiat_element, polynomial_order, quadrature_degree
 
     # Runtime quadrature
     L = L_eqn * ufl.dx(metadata={"quadrature_rule": "runtime"})
-    cells = np.arange(1)
+    num_cells = mesh.topology.index_map(mesh.topology.dim).size_local
+    cells = np.arange(num_cells)
     q = FIAT.create_quadrature(fiat_element, quadrature_degree)
-    qr_pts = q.get_points().flatten()
-    qr_w = q.get_weights().flatten()
+    qr_pts = np.tile(q.get_points().flatten(), [num_cells, 1])
+    qr_w = np.tile(q.get_weights().flatten(), [num_cells, 1])
     qr_n = qr_pts  # dummy
-    b = libcutfemx.custom_assemble_vector(L, [(cells, [qr_pts], [qr_w], [qr_n])])
+    b = libcutfemx.custom_assemble_vector(L, [(cells, qr_pts, qr_w, qr_n)])
 
     # Reference
     L_ref = L_eqn * ufl.dx
@@ -50,34 +51,18 @@ def rhs4(x):
     return exp(x[0] * x[1])
 
 
-# @pytest.mark.parametrize("cell", ["quadrilateral", "hexahedron"])
-# @pytest.mark.parametrize(
-#     ("polynomial_order", "quadrature_degree"),
-#     [(1, 2)],  # , (2, 2), (3, 2), (4, 3), (5, 4)]
-# )
-# @pytest.mark.parametrize("xmin", [[0, 0, 0], [-0.25, -1.25, 0.25]])
-# @pytest.mark.parametrize("xmax", [[1, 1, 1], [1.25, 7.5, 4.4]])
-# @pytest.mark.parametrize("rhs", [rhs1, rhs2, rhs3, rhs4])
-# def test_tensor_product_element(
-#     cell, polynomial_order, quadrature_degree, xmin, xmax, rhs
-# ):
-#     b, b_ref = single_tensor_product_element(
-#         cell, polynomial_order, quadrature_degree, xmin, xmax, rhs
-#     )
-#     assert np.linalg.norm(b.array - b_ref.array) / np.linalg.norm(b_ref.array) < 1e-10
-
-
+@pytest.mark.parametrize("N", [[1, 1], [3, 1], [1, 3], [3, 4]])
 @pytest.mark.parametrize("xmin", [[0, 0], [-0.25, -10.25]])
 @pytest.mark.parametrize("xmax", [[1, 1], [1.25, 17.5]])
 @pytest.mark.parametrize("rhs", [rhs1, rhs2, rhs3, rhs4])
-def test_single_linear_quad(xmin, xmax, rhs):
+def test_linear_quads(N, xmin, xmax, rhs):
     polynomial_order = 1
     quadrature_degree = 2
     cell_type = dolfinx.mesh.CellType.quadrilateral
     mesh = dolfinx.mesh.create_rectangle(
         MPI.COMM_WORLD,
         np.array([xmin, xmax]),
-        np.array([1, 1]),
+        np.array(N),
         cell_type,
     )
     fiat_element = FIAT.reference_element.UFCQuadrilateral()
@@ -88,17 +73,18 @@ def test_single_linear_quad(xmin, xmax, rhs):
     assert np.linalg.norm(b.array - b_ref.array) / np.linalg.norm(b_ref.array) < 1e-10
 
 
+@pytest.mark.parametrize("N", [[1, 1, 1], [3, 1, 1], [1, 3, 1], [1, 1, 3], [3, 4, 5]])
 @pytest.mark.parametrize("xmin", [[0, 0, 0], [-0.25, -10.25, 0.25]])
 @pytest.mark.parametrize("xmax", [[1, 1, 1], [1.25, 17.5, 4.4]])
 @pytest.mark.parametrize("rhs", [rhs1, rhs2, rhs3, rhs4])
-def test_single_linear_hex(xmin, xmax, rhs):
+def test_linear_hexes(N, xmin, xmax, rhs):
     polynomial_order = 1
     quadrature_degree = 2
     cell_type = dolfinx.mesh.CellType.hexahedron
     mesh = dolfinx.mesh.create_box(
         MPI.COMM_WORLD,
         np.array([xmin, xmax]),
-        np.array([1, 1, 1]),
+        np.array(N),
         cell_type,
     )
     fiat_element = FIAT.reference_element.UFCHexahedron()
@@ -107,3 +93,7 @@ def test_single_linear_hex(xmin, xmax, rhs):
         mesh, fiat_element, polynomial_order, quadrature_degree, rhs
     )
     assert np.linalg.norm(b.array - b_ref.array) / np.linalg.norm(b_ref.array) < 1e-10
+
+
+def test_normals():
+    pass
