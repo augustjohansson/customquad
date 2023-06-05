19f202a4eea940a7d84bbf890dd4fc3304541710
August Johansson <august.johansson@sintef.no>
2023-06-05 14:06:58 +0200

    Rename

--- src/customquad/__init__.py
+++ src/customquad/__init__.py
@@ -1,3 +1,3 @@
-from .assemble_matrix import assemble_matrix, custom_assemble_matrix
-from .assemble_vector import assemble_vector, custom_assemble_vector
-from .assemble_scalar import assemble_scalar, custom_assemble_scalar
+from .assemble_matrix import assemble_matrix
+from .assemble_vector import assemble_vector
+from .assemble_scalar import assemble_scalar
