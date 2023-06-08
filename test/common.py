6bbd68849aa33d40cb65f3d902436df68131dec1
August Johansson <august.johansson@sintef.no>
2023-06-08 19:43:42 +0200

    Move mesh routine to common

--- test/common.py
+++ test/common.py
@@ -106,3 +106,68 @@ def assemble_matrix_test(mesh, fiat_element, polynomial_order, quadrature_degree
     A_ref.assemble()
 
     return A, A_ref
+
+
+def get_mesh():
+    # Mesh
+    N = 10
+    cell_type = dolfinx.mesh.CellType.quadrilateral
+    # mesh = dolfinx.mesh.create_unit_square(MPI.COMM_WORLD, N, N, cell_type)
+    xmin = np.array([-1.23, -11.11])
+    xmax = np.array([3.33, 99.99])
+    mesh = dolfinx.mesh.create_rectangle(
+        MPI.COMM_WORLD, np.array([xmin, xmax]), np.array([N, N]), cell_type=cell_type
+    )
+    h = (xmax - xmin) / N
+
+    # Classify cells
+    dim = mesh.topology.dim
+    num_cells = mesh.topology.index_map(dim).size_local
+    all_cells = np.arange(num_cells, dtype=np.int32)
+    ge = dolfinx.cpp.mesh.entities_to_geometry(mesh, dim, all_cells, False)
+    centroids = np.mean(mesh.geometry.x[ge], axis=1)
+    xc = centroids[:, 0]
+    yc = centroids[:, 1]
+
+    left = xc < xmin[0] + h[0]
+    right = xc > xmax[0] - h[0]
+    bottom = yc < xmin[1] + h[1]
+    top = yc > xmax[1] - h[1]
+    outside_cells = np.where(np.logical_or.reduce((left, right, bottom, top)))[0]
+    assert len(outside_cells) == 4 * (N - 1)
+
+    left = xc < xmin[0] + 2 * h[0]
+    right = xc > xmax[0] - 2 * h[0]
+    bottom = yc < xmin[1] + 2 * h[1]
+    top = yc > xmax[1] - 2 * h[1]
+    cut_cells = np.where(np.logical_or.reduce((left, right, bottom, top)))[0]
+    cut_cells = np.setdiff1d(cut_cells, outside_cells)
+    assert len(cut_cells) == 4 * (N - 3)
+
+    uncut_cells = np.setdiff1d(all_cells, outside_cells)
+    uncut_cells = np.setdiff1d(uncut_cells, cut_cells)
+    assert len(uncut_cells) == (N - 4) ** 2
+
+    # Setup mesh tags
+    cut_cell_tag = 1
+    uncut_cell_tag = 2
+    outside_cell_tag = 3
+
+    celltags = cq.utils.get_celltags(
+        mesh,
+        cut_cells,
+        uncut_cells,
+        outside_cells,
+        cut_cell_tag=cut_cell_tag,
+        uncut_cell_tag=uncut_cell_tag,
+        outside_cell_tag=outside_cell_tag,
+    )
+
+    return (
+        mesh,
+        h,
+        celltags,
+        cut_cell_tag,
+        uncut_cell_tag,
+        outside_cell_tag,
+    )
