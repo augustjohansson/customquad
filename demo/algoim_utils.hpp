2c2797bbdcd83e21c17740d4c07312a9d9ebb8dc
August Johansson <august.johansson@sintef.no>
2023-07-12 15:49:05 +0200

    Separate algoim stuff

---
+++ demo/algoim_utils.hpp
@@ -0,0 +1,216 @@
+#include <vector>
+#include "quadrature_general.hpp"
+
+namespace algoim_utils
+{
+
+  template <class T>
+  void convert(const T& qr,
+	       const int gdim,
+	       std::vector<double>& pts,
+	       std::vector<double>& w)
+  {
+    const std::size_t N = qr.nodes.size();
+    pts.resize(gdim * N);
+    w.resize(N);
+    for (std::size_t i = 0; i < N; ++i) {
+      for (int d = 0; d < gdim; ++d)
+	pts[gdim * i + d] = qr.nodes[i].x(d);
+      w[i] = qr.nodes[i].w;
+    }
+  }
+  
+  template<class Y, int gdim>
+  void run_template(const Y& phi,
+		    const std::vector<double>& LLx,
+		    const std::vector<double>& LLy,
+		    const std::vector<double>& LLz,
+		    const std::vector<double>& URx,
+		    const std::vector<double>& URy,
+		    const std::vector<double>& URz,
+		    int degree,
+		    bool do_verbose,
+		    bool do_map,
+		    bool do_scale,
+		    std::vector<int>& cut_cells,
+		    std::vector<int>& uncut_cells,
+		    std::vector<int>& outside_cells,
+		    std::vector<std::vector<double>>& qr_pts,
+		    std::vector<std::vector<double>>& qr_w,
+		    std::vector<std::vector<double>>& qr_pts_bdry,
+		    std::vector<std::vector<double>>& qr_w_bdry,
+		    std::vector<std::vector<double>>& qr_n,
+		    std::vector<std::vector<double>>& xyz,
+		    std::vector<std::vector<double>>& xyz_bdry)
+  {
+    std::cout << "algoim_utils " << __FILE__ << ' ' << __FUNCTION__ << std::endl;
+
+    if (do_verbose) {
+      std::cout << "LLx " << LLx.size() << '\n'
+		<< "LLy " << LLy.size() << '\n';
+      if (gdim == 3)
+	std::cout << "LLz " << LLz.size() << '\n';
+      std::cout << "URx " << URx.size() << '\n'
+		<< "URy " << URy.size() << '\n';
+      if (gdim == 3)
+	std::cout << "URz " << URz.size() << '\n';
+      std::cout << "degree " << degree << '\n'
+		<< "do_verbose " << do_verbose << '\n'
+		<< "map " << do_map << '\n'
+		<< "do_scale " << do_scale << std::endl;
+    }
+
+    const int side = -1;
+    const int dim_bulk = -1;
+
+    const std::size_t num_cells = LLx.size();
+    assert(num_cells == LLy.size());
+    if (gdim == 3)
+      assert(num_cells == LLz.size());
+    assert(num_cells == URx.size());
+    assert(num_cells == URy.size());
+    if (gdim == 3)
+      assert(num_cells == URz.size());
+    qr_pts.resize(num_cells);
+    qr_w.resize(num_cells);
+    qr_pts_bdry.resize(num_cells);
+    qr_w_bdry.resize(num_cells);
+    qr_n.resize(num_cells);
+    xyz.resize(num_cells);
+    xyz_bdry.resize(num_cells);
+
+    for (std::size_t cell_no = 0; cell_no < num_cells; ++cell_no) {
+      algoim::uvector<double, gdim> min, max;
+      min(0) = LLx[cell_no];
+      min(1) = LLy[cell_no];
+      max(0) = URx[cell_no];
+      max(1) = URy[cell_no];
+      if (gdim == 3) {
+	min(2) = LLz[cell_no];
+	max(2) = URz[cell_no];
+      }
+    
+      const auto q_bdry = algoim::quadGen<gdim>
+	(phi, algoim::HyperRectangle<double, gdim>(min, max), gdim, side, degree);
+    
+      if (q_bdry.nodes.size()) {
+	// Cut cell
+	cut_cells.push_back(cell_no);
+
+	// Boundary qr
+	convert(q_bdry, gdim, qr_pts_bdry[cell_no], qr_w_bdry[cell_no]);
+	xyz_bdry[cell_no] = qr_pts_bdry[cell_no];
+
+	// Normal
+	for (std::size_t k = 0; k < qr_pts_bdry[cell_no].size() / gdim; ++k) {
+	  algoim::uvector<double, gdim> pt;
+	  for (std::size_t d = 0; d < gdim; ++d)
+	    pt(d) = qr_pts_bdry[cell_no][gdim * k + d];
+	  const algoim::uvector<double, gdim> grad = phi.grad(pt);
+	  for (std::size_t d = 0; d < gdim; ++d)
+	    qr_n[cell_no].push_back(grad(d)); // FIXME
+	}
+
+	// Bulk
+	const auto q_bulk = algoim::quadGen<gdim>
+	  (phi, algoim::HyperRectangle<double, gdim>(min, max), dim_bulk, side, degree);
+	convert(q_bulk, gdim, qr_pts[cell_no], qr_w[cell_no]);
+	xyz[cell_no] = qr_pts[cell_no];
+
+	const algoim::uvector<double, gdim> dx(max - min);
+	double vol = 1;
+	for (std::size_t d = 0; d < gdim; ++d)
+	  vol *= dx(d);
+      
+	if (do_scale) {
+	  for (double& w: qr_w[cell_no])
+	    w /= vol;
+	  for (double& w: qr_w_bdry[cell_no])
+	    w /= vol;
+	}
+
+	if (do_map) {
+	  auto map = [&](std::vector<double>& qr) {
+	    for (std::size_t i = 0; i < qr.size(); i += gdim) {
+	      for (std::size_t d = 0; d < gdim; ++d)
+		qr[i + d] = (qr[i + d] - min(d)) / dx(d);
+	    }
+	  };
+	  map(qr_pts[cell_no]);
+	  map(qr_pts_bdry[cell_no]);
+	}
+      }
+      else {
+	// Either outside or inside
+	const algoim::uvector<double, gdim> midp(0.5 * (min + max));
+	if (phi(midp) < 0)
+	  uncut_cells.push_back(cell_no);
+	else
+	  outside_cells.push_back(cell_no);
+      }
+    }
+
+    for (std::size_t i = 0; i < num_cells; ++i) {
+      if (qr_w_bdry[i].size()) {
+	assert(qr_pts[i].size());
+	assert(qr_w[i].size());
+	assert(qr_pts[i].size() == gdim * qr_w[i].size());
+
+	assert(qr_pts_bdry[i].size());
+	assert(qr_w_bdry[i].size());
+	assert(qr_n[i].size());
+	assert(qr_pts_bdry[i].size() == gdim * qr_w_bdry[i].size());
+	assert(qr_pts_bdry[i].size() == qr_n[i].size());
+
+	assert(xyz[i].size());
+	assert(xyz_bdry[i].size());
+      }
+      else {
+	assert(qr_pts[i].size() == 0);
+	assert(qr_w[i].size() == 0);
+	assert(qr_pts_bdry[i].size() == 0);
+	assert(qr_w_bdry[i].size() == 0);
+	assert(qr_n[i].size() == 0);
+	assert(xyz[i].size() == 0);
+	assert(xyz_bdry[i].size() == 0);
+      }
+    }
+
+    if (do_verbose) {
+      auto print = [](const std::vector<std::vector<double>>& p) {
+	for (std::size_t i = 0; i < p.size(); ++i)
+	  for (std::size_t j = 0; j < p[i].size(); j += gdim) {
+	    for (std::size_t d = 0; d < gdim; ++d)
+	      std::cout << p[i][j+d] << ' ';
+	    std::cout << '\n';
+	  }
+      };
+
+      std::cout << "% bulk [0,1]^2\n"
+		<<"qr_pts=[";
+      print(qr_pts);
+      std::cout << "];\n";
+
+      std::cout << "% bdry [0,1]^2\n"
+		<< "qr_pts_bdry=[";
+      print(qr_pts_bdry);
+      std::cout << "];\n";
+
+      std::cout << "xyz=[";
+      print(xyz);
+      std::cout << "];\n";
+
+      std::cout << "xyz_bdry=[";
+      print(xyz_bdry);
+      std::cout << "];\n";
+
+      std::cout << "qr_n=[";
+      print(qr_n);
+      std::cout << "];\n";
+
+      std::cout << "drawarrow(xyz_bdry, xyz_bdry+qr_n);\n";
+    }
+
+    std::cout << __FILE__ << " done" << std::endl;
+  }
+}
--- demo/circle.hpp
+++ demo/circle.hpp
@@ -1,5 +1,4 @@
-#include <cassert>
-#include <fstream>
+#include "algoim_utils.hpp"
 #include "quadrature_general.hpp"
 
 template <int gdim>
@@ -54,189 +53,24 @@ std::vector<std::vector<double>> get_qr_n() { return qr_n; }
 std::vector<std::vector<double>> get_xyz() { return xyz; }
 std::vector<std::vector<double>> get_xyz_bdry() { return xyz_bdry; }
 
-template <class T>
-void convert(const T& qr, const int gdim, std::vector<double>& pts,
-             std::vector<double>& w)
-{
-  const std::size_t N = qr.nodes.size();
-  pts.resize(gdim * N);
-  w.resize(N);
-  for (std::size_t i = 0; i < N; ++i)
-  {
-    for (int d = 0; d < gdim; ++d)
-      pts[gdim * i + d] = qr.nodes[i].x(d);
-    w[i] = qr.nodes[i].w;
-  }
-}
-
-void run(const std::vector<double>& LLx, const std::vector<double>& LLy,
-         const std::vector<double>& LLz, const std::vector<double>& URx,
-         const std::vector<double>& URy, const std::vector<double>& URz,
-         int degree, bool verbose, bool map, bool scale)
+void run(const std::vector<double>& LLx,
+	 const std::vector<double>& LLy,
+         const std::vector<double>& LLz,
+	 const std::vector<double>& URx,
+         const std::vector<double>& URy,
+	 const std::vector<double>& URz,
+         int degree, bool do_verbose, bool do_map, bool do_scale)
 {
   std::cout << "algoim " << __FILE__ << std::endl;
 
-  if (verbose)
-  {
-    std::cout << "LLx " << LLx.size() << '\n'
-              << "LLy " << LLy.size() << '\n'
-              << "URx " << URx.size() << '\n'
-              << "URy " << URy.size() << '\n'
-              << "degree " << degree << '\n'
-              << "verbose " << verbose << '\n'
-              << "map " << map << '\n'
-              << "scale " << scale << std::endl;
-  }
-
-  const int gdim = 2;
   Sphere<2> phi;
-
-  const int side = -1;
-  const int dim_bulk = -1;
-  const int dim_bdry = gdim;
-
-  const std::size_t num_cells = LLx.size();
-  assert(num_cells == LLy.size());
-  assert(num_cells == URx.size());
-  assert(num_cells == URy.size());
-  qr_pts.resize(num_cells);
-  qr_w.resize(num_cells);
-  qr_pts_bdry.resize(num_cells);
-  qr_w_bdry.resize(num_cells);
-  qr_n.resize(num_cells);
-  xyz.resize(num_cells);
-  xyz_bdry.resize(num_cells);
-
-  for (std::size_t cell_no = 0; cell_no < num_cells; ++cell_no)
-  {
-    const algoim::uvector<double, 2> min(LLx[cell_no], LLy[cell_no]);
-    const algoim::uvector<double, 2> max(URx[cell_no], URy[cell_no]);
-
-    const auto q_bdry = algoim::quadGen<2>(
-        phi, algoim::HyperRectangle<double, 2>(min, max), dim_bdry, side, degree);
-    if (q_bdry.nodes.size())
-    {
-      // Cut cell
-      cut_cells.push_back(cell_no);
-
-      // Boundary qr
-      convert(q_bdry, gdim, qr_pts_bdry[cell_no], qr_w_bdry[cell_no]);
-      xyz_bdry[cell_no] = qr_pts_bdry[cell_no];
-
-      // Normal
-      for (std::size_t k = 0; k < qr_pts_bdry[cell_no].size() / gdim; ++k)
-      {
-        const double x = qr_pts_bdry[cell_no][gdim * k];
-        const double y = qr_pts_bdry[cell_no][gdim * k + 1];
-        const auto grad = phi.grad(algoim::uvector<double, 2>(x, y));
-        for (std::size_t d = 0; d < gdim; ++d)
-          qr_n[cell_no].push_back(grad(d));
-      }
-
-      // Bulk
-      const auto q_bulk = algoim::quadGen<2>(
-          phi, algoim::HyperRectangle<double, 2>(min, max), dim_bulk, side, degree);
-      convert(q_bulk, gdim, qr_pts[cell_no], qr_w[cell_no]);
-      xyz[cell_no] = qr_pts[cell_no];
-
-      if (scale)
-      {
-        // fenics scales with volume, need to compensate for this
-        const double dx = URx[cell_no] - LLx[cell_no];
-        const double dy = URy[cell_no] - LLy[cell_no];
-        const double vol = dx * dy;
-        for (double& w : qr_w[cell_no])
-          w /= vol;
-        for (double& w : qr_w_bdry[cell_no])
-          w /= vol;
-      }
-
-      if (map)
-      {
-        // Map to [0,1]^2
-        const double dx = URx[cell_no] - LLx[cell_no];
-        const double dy = URy[cell_no] - LLy[cell_no];
-        auto map = [&](std::vector<double>& qr) {
-          for (std::size_t i = 0; i < qr.size(); i += 2)
-          {
-            qr[i] = (qr[i] - LLx[cell_no]) / dx;
-            qr[i + 1] = (qr[i + 1] - LLy[cell_no]) / dy;
-          }
-        };
-
-        map(qr_pts[cell_no]);
-        map(qr_pts_bdry[cell_no]);
-      }
-    }
-    else
-    {
-      // Either outside or inside
-      const algoim::uvector<double, 2> midp = 0.5 * (min + max);
-      if (phi(midp) < 0)
-        uncut_cells.push_back(cell_no);
-      else
-        outside_cells.push_back(cell_no);
-    }
-  }
-
-  for (std::size_t i = 0; i < num_cells; ++i)
-  {
-    if (qr_w_bdry[i].size())
-    {
-      assert(qr_pts[i].size());
-      assert(qr_w[i].size());
-      assert(qr_pts[i].size() == gdim * qr_w[i].size());
-
-      assert(qr_pts_bdry[i].size());
-      assert(qr_w_bdry[i].size());
-      assert(qr_n[i].size());
-      assert(qr_pts_bdry[i].size() == gdim * qr_w_bdry[i].size());
-      assert(qr_pts_bdry[i].size() == qr_n[i].size());
-
-      assert(xyz[i].size());
-      assert(xyz_bdry[i].size());
-    }
-    else
-    {
-      assert(qr_pts[i].size() == 0);
-      assert(qr_w[i].size() == 0);
-      assert(qr_pts_bdry[i].size() == 0);
-      assert(qr_w_bdry[i].size() == 0);
-      assert(qr_n[i].size() == 0);
-      assert(xyz[i].size() == 0);
-      assert(xyz_bdry[i].size() == 0);
-    }
-  }
-
-  if (verbose)
-  {
-    auto print = [](const std::vector<std::vector<double>>& p) {
-      for (std::size_t i = 0; i < p.size(); ++i)
-        for (std::size_t j = 0; j < p[i].size(); j += 2)
-          std::cout << p[i][j] << ' ' << p[i][j + 1] << '\n';
-    };
-
-    std::cout << "bulk [0,1]^2\n";
-    std::cout <<"qr_pts=[";
-    print(qr_pts);
-    std::cout << "];\n";
-
-    std::cout << "bdry [0,1]^2\n";
-    std::cout << "qr_pts_bdry=[";
-    print(qr_pts_bdry);
-    std::cout << "];\n";
-    
-    std::cout << "xyz=[";
-    print(xyz);
-    std::cout << "];\n";
-    std::cout << "xyz_bdry=[";
-    print(xyz_bdry);
-    std::cout << "];\n";
-    std::cout << "qr_n=[";
-    print(qr_n);
-    std::cout << "];\n";
-    std::cout << "drawarrow(xyz_bdry, xyz_bdry+qr_n);\n";
-  }
-
-  std::cout << __FILE__ << " done" << std::endl;
+  
+  algoim_utils::run_template<Sphere<2>, 2>
+    (phi,
+     LLx, LLy, LLz, URx, URy, URz, degree,
+     do_verbose, do_map, do_scale,
+     cut_cells, uncut_cells, outside_cells,
+     qr_pts, qr_w,
+     qr_pts_bdry, qr_w_bdry, qr_n,
+     xyz, xyz_bdry);
 }
