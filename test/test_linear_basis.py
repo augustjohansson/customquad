a5eb60088cf1f27b821e5aa888ef55cb2f48e451
August Johansson <august.johansson@sintef.no>
2023-07-14 13:57:05 +0200

    Hex face integral test

--- test/test_linear_basis.py
+++ test/test_linear_basis.py
@@ -212,6 +212,127 @@ def test_edge_integral():
             assert abs(b.array[m] - b_exact.array[m]) < 1e-10
 
 
+def test_face_integral():
+    # Test bdry integral with basis function
+    N = 1
+    cell_type = dolfinx.mesh.CellType.hexahedron
+    bbmin = np.array([-0.25, -10.25, -4.4])
+    bbmax = np.array([1.25, 17.5, 0.1])
+    mesh = dolfinx.mesh.create_box(
+        MPI.COMM_WORLD,
+        np.array([bbmin, bbmax]),
+        np.array([N, N, N]),
+        cell_type=cell_type,
+    )
+    tdim = mesh.topology.dim
+    num_cells = cq.utils.get_num_cells(mesh)
+    cells = np.arange(num_cells)
+
+    V = dolfinx.fem.FunctionSpace(mesh, ("Lagrange", 1))
+    v = ufl.TestFunction(V)
+    integrand = 1 * v
+
+    # fmt: off
+    def xmin(x): return np.isclose(x[0], bbmin[0])
+    def xmax(x): return np.isclose(x[0], bbmax[0])
+    def ymin(x): return np.isclose(x[1], bbmin[1])
+    def ymax(x): return np.isclose(x[1], bbmax[1])
+    def zmin(x): return np.isclose(x[2], bbmin[2])
+    def zmax(x): return np.isclose(x[2], bbmax[2])
+    # fmt: on
+
+    xmin_tag = 10
+    xmin_facets = dolfinx.mesh.locate_entities_boundary(mesh, tdim - 1, xmin)
+    xmin_vals = np.full(xmin_facets.shape, xmin_tag, np.intc)
+
+    xmax_tag = 11
+    xmax_facets = dolfinx.mesh.locate_entities_boundary(mesh, tdim - 1, xmax)
+    xmax_vals = np.full(xmax_facets.shape, xmax_tag, np.intc)
+
+    ymin_tag = 12
+    ymin_facets = dolfinx.mesh.locate_entities_boundary(mesh, tdim - 1, ymin)
+    ymin_vals = np.full(ymin_facets.shape, ymin_tag, np.intc)
+
+    ymax_tag = 13
+    ymax_facets = dolfinx.mesh.locate_entities_boundary(mesh, tdim - 1, ymax)
+    ymax_vals = np.full(ymax_facets.shape, ymax_tag, np.intc)
+
+    zmin_tag = 14
+    zmin_facets = dolfinx.mesh.locate_entities_boundary(mesh, tdim - 1, zmin)
+    zmin_vals = np.full(zmin_facets.shape, zmin_tag, np.intc)
+
+    zmax_tag = 15
+    zmax_facets = dolfinx.mesh.locate_entities_boundary(mesh, tdim - 1, zmax)
+    zmax_vals = np.full(zmax_facets.shape, zmax_tag, np.intc)
+
+    indices = np.hstack(
+        (xmin_facets, xmax_facets, ymin_facets, ymax_facets, zmin_facets, zmax_facets)
+    )
+    values = np.hstack(
+        (xmin_vals, xmax_vals, ymin_vals, ymax_vals, zmin_vals, zmax_vals)
+    )
+
+    indices, pos = np.unique(indices, return_index=True)
+    marker = dolfinx.mesh.meshtags(mesh, tdim - 1, indices, values[pos])
+    ds = ufl.Measure("ds", subdomain_data=marker)
+    dx = ufl.dx(metadata={"quadrature_rule": "runtime"})
+
+    mesh.topology.create_connectivity(tdim - 1, 0)
+    f2n = mesh.topology.connectivity(tdim - 1, 0)
+    xmin_nodes = f2n.links(xmin_facets)
+    xmax_nodes = f2n.links(xmax_facets)
+    ymin_nodes = f2n.links(ymin_facets)
+    ymax_nodes = f2n.links(ymax_facets)
+    zmin_nodes = f2n.links(zmin_facets)
+    zmax_nodes = f2n.links(zmax_facets)
+
+    # Complicated way of calculating qr_pts, but this illustrates the numbering
+    qr_pts_ref = np.array(
+        [
+            [0.0, 0.5, 0.5],
+            [1.0, 0.5, 0.5],
+            [0.5, 0.0, 0.5],
+            [0.5, 1.0, 0.5],
+            [0.5, 0.5, 0.0],
+            [0.5, 0.5, 1.0],
+        ]
+    )
+    xdiff = bbmax - bbmin
+    x = mesh.geometry.x[:, 0:tdim]
+    num_faces = 6
+    qr_pts = np.empty((num_faces, tdim))
+    qr_pts[0] = (np.mean(x[xmin_nodes], axis=0) - bbmin) / xdiff
+    qr_pts[1] = (np.mean(x[xmax_nodes], axis=0) - bbmin) / xdiff
+    qr_pts[2] = (np.mean(x[ymin_nodes], axis=0) - bbmin) / xdiff
+    qr_pts[3] = (np.mean(x[ymax_nodes], axis=0) - bbmin) / xdiff
+    qr_pts[4] = (np.mean(x[zmin_nodes], axis=0) - bbmin) / xdiff
+    qr_pts[5] = (np.mean(x[zmax_nodes], axis=0) - bbmin) / xdiff
+    for k in range(num_faces):
+        assert np.all(abs(qr_pts[k] - qr_pts_ref[k]) < 1e-10)
+
+    facet_size = np.empty(num_faces)
+    facet_size[0] = facet_size[1] = xdiff[1] * xdiff[2]
+    facet_size[2] = facet_size[3] = xdiff[0] * xdiff[2]
+    facet_size[4] = facet_size[5] = xdiff[0] * xdiff[1]
+    cell_volume = np.prod(xdiff)
+    qr_w = facet_size / cell_volume
+
+    tags = [xmin_tag, xmax_tag, ymin_tag, ymax_tag, zmin_tag, zmax_tag]
+    for k in range(num_faces):
+        qr_pts_local = np.expand_dims(qr_pts[k], axis=0)
+        qr_w_local = np.expand_dims(qr_w[k], axis=(0, 1))
+        qr_data = [(cells, qr_pts_local, qr_w_local)]
+        b = cq.assemble_vector(dolfinx.fem.form(integrand * dx), qr_data)
+
+        ds_local = ds(tags[k])
+        b_exact = dolfinx.fem.petsc.assemble_vector(
+            dolfinx.fem.form(integrand * ds_local)
+        )
+
+        for m, v in enumerate(b.array):
+            assert abs(v - b_exact.array[m]) < 1e-10
+
+
 # def test_edges3():
 #     # One edge at a time
 #     N = 1
