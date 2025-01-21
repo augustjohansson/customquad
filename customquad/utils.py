96bad8727cce4bfcb5f33ea6101c68b8e084e4d6
August Johansson <august.johansson@sintef.no>
2025-01-21 09:56:31 +0100

    Simplify utils

--- customquad/utils.py
+++ customquad/utils.py
@@ -219,15 +219,63 @@ def writeXDMF(filename, mesh, data):
             raise RuntimeError("Unsupported data when writing file", filename)
 
 
-def subdomain(qr_data, num_cells):
-    # In the case of integration over subdomains,
-    # eg. ds_cut(cut_cell_tag), the coeffs are already defined over
-    # the subdomain with id=cut_cell_tag. Hence we need to renumber
-    # the cells in the qr_data to match this. Note that the provided
-    # quadrature rule must match the number of cells in this
-    # subdomain.
+def print_for_header(
+    b_local,
+    coeffs,
+    constants,
+    cell_coords,
+    entity_local_index,
+    quadrature_permutation,
+    num_quadrature_points,
+    qr_pts,
+    qr_w,
+    qr_n,
+):
+    def print_flat(x):
+        print("{", end="")
+        for xi in x:
+            print(xi, end=",")
+        print("};")
 
-    idx = np.arange(num_cells)
-    qr_data2 = [(idx,) + qr[1:] for qr in qr_data]
+    print("printing function params:")
+    print("double A[] = ", end="")
+    print_flat(b_local)
+    print("const double w[] = ", end="")
+    print_flat(coeffs)
+    print("const double c[] = ", end="")
+    print_flat(constants)
+    print("const double coordinate_dofs[] = ", end="")
+    print_flat(cell_coords.flatten())
+    print("const int entity_local_index[] = ", end="")
+    print_flat(entity_local_index.flatten())
+    print("const uint8_t quadrature_permutation[] = ", end="")
+    print_flat(quadrature_permutation.flatten())
+    print("const int num_quadrature_points = ", num_quadrature_points, ";")
+    print("const double quadrature_points[] = ", end="")
+    print_flat(qr_pts)
+    print("const double quadrature_weights[] = ", end="")
+    print_flat(qr_w)
+    print("const double facet_normals[] = ", end="")
+    print_flat(qr_n)
+    print(
+        "tabulate_tensor_integral_custom_otherwise(A,w,c,coordinate_dofs,entity_local_index,quadrature_permutation,num_quadrature_points,quadrature_points,quadrature_weights,facet_normals);"
+    )
 
-    return qr_data2
+
+def dump(filename, A, do_print=False):
+    print(f"dump to {filename}")
+
+    if isinstance(A, PETSc.Mat):
+        assert A.assembled
+        print(A.size)
+        with open(filename, "w") as f:
+            for r in range(A.size[0]):
+                cols, vals = A.getRow(r)
+                for col, val in zip(cols, vals):
+                    s = str(r + 1) + " " + str(col + 1) + " " + str(val) + "\n"
+                    f.write(s)
+                    if do_print:
+                        print(s, end="")
+    else:
+        print(len(A.array))
+        np.savetxt(filename, A.array)
